2b7c746b7083c4989f1e32abc6497132e2313e1a
Stuart Gardner <stuarttgardner@gmail.com>
2019-01-28 13:47:24 -0700

    finished

--- exercises/if-statement-olympics/app.js
+++ exercises/if-statement-olympics/app.js
@@ -20,3 +20,35 @@ if (person.age >= 18 && person.name.startsWith("B")){
 else {
     console.log(person.name + " is not allowed to go to the movies" )
 }
+
+//Silver Medal
+if (1 === "1") {
+    console.log('strict')
+} else if (1 == "1") {
+    console.log('loose')
+} else {
+    console.log('not equal at all')
+}
+if (1 <= 2 || 2 == 4) {
+    console.log('yes')
+}
+
+//Gold Medal
+if(typeof "dog" === 'string') {
+    console.log('string')
+}
+if(typeof "true" === 'boolean') {
+    console.log('boolean')
+}
+var num = 72
+if(typeof num !== 'undefined') {
+    console.log('defined')
+}
+if("s" > 12) {
+    console.log("probably not")
+}
+var myNum = 10
+myNum % 2 === 0 ? console.log('even') : console.log('odd')
+
+var myNum = 11
+myNum % 2 === 0 ? console.log('even') : console.log('odd')
